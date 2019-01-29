fc6fda300fddb25f2b2045b860b18c583a9e4e30
Martin Juhl <m@rtinjuhl.dk>
2019-01-29 08:41:06 +0000

    fifixx

--- buildhelpers/build.sh
+++ buildhelpers/build.sh
@@ -8,7 +8,7 @@ yum install -y rpmdevtools yum-utils fakeroot centos-release-scl
 yum install -y wget
 wget -O /etc/yum.repos.d/ansible-awx.repo https://copr.fedorainfracloud.org/coprs/mrmeee/ansible-awx/repo/epel-7/mrmeee-ansible-awx-epel-7.repo
 
-yum -y install python3-rpm-macros python-rpm-macros rh-python36-build rh-python36-python rh-python36-python-devel gcc make libffi-devel openssl-devel libxml2-devel libxslt-devel gcc-c++ rh-postgresql10-postgresql-devel openldap-devel xmlsec1-devel libxml2-devel xmlsec1-openssl-devel libtool-ltdl-devel rh-python36-python-sphinx
+yum -y install python3-rpm-macros python-rpm-macros rh-python36-build rh-python36-python rh-python36-python-devel gcc make libffi-devel openssl-devel libxml2-devel libxslt-devel gcc-c++ rh-postgresql10-postgresql-devel openldap-devel xmlsec1-devel libxml2-devel xmlsec1-openssl-devel libtool-ltdl-devel rh-python36-python-sphinx scl-utils-build centos-release-sclv libcurl-devel rh-python36-pycparser rh-python36-pbr rh-python36-cffi python3-rpm-macros python-rpm-macros rh-python36-build rh-python36-setuptools_scm rh-python36-python rh-python36-python-devel gcc make libffi-devel openssl-devel libxml2-devel libxslt-devel gcc-c++ rh-postgresql10-postgresql-devel openldap-devel xmlsec1-devel libxml2-devel xmlsec1-openssl-devel libtool-ltdl-devel rh-python36-python-sphinx rh-python36-six
 
 ln -s /opt/rh/rh-python36/root/usr/bin/python3 /usr/bin/python3.6
 
