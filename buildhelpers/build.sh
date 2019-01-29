7a2bdd6e380f0228356f9a5b7bee7fb4937ff327
Martin Juhl <m@rtinjuhl.dk>
2019-01-29 00:58:33 +0000

    build script updated

--- buildhelpers/build.sh
+++ buildhelpers/build.sh
@@ -5,8 +5,20 @@ set -x
 SPECFILE="/source/$1"
 
 yum install -y rpmdevtools yum-utils fakeroot centos-release-scl
+yum install -y wget
+wget -O /etc/yum.repos.d/ansible-awx.repo https://copr.fedorainfracloud.org/coprs/mrmeee/ansible-awx/repo/epel-7/mrmeee-ansible-awx-epel-7.repo
 
-ln -s /usr/bin/python36 /usr/bin/python3
+yum -y install python3-rpm-macros python-rpm-macros rh-python36-build rh-python36-python rh-python36-python-devel gcc make libffi-devel openssl-devel libxml2-devel libxslt-devel gcc-c++ rh-postgresql10-postgresql-devel openldap-devel xmlsec1-devel libxml2-devel xmlsec1-openssl-devel libtool-ltdl-devel rh-python36-python-sphinx rh-python36-python-six
+
+ln -s /opt/rh/rh-python36/root/usr/bin/python3 /usr/bin/python3.6
+
+ln -s /opt/rh/rh-python36/root/usr/bin/sphinx-build /opt/rh/rh-python36/root/usr/bin/sphinx-build-3
+
+ln -s /opt/rh/rh-postgresql10/root/usr/bin/pg_config /usr/bin/pg_config
+
+ln -s /usr/include/libxml2/libxml /usr/include/libxml
+
+export LC_CTYPE=en_US.UTF-8
 
 useradd -s /bin/bash builder || true
 chown builder. /cache -R
